f264a79100177d353bf3f14c53e39f7b60eae533
Peter Menzies <petermenzies@bren.ucsb.edu>
2021-08-29 10:34:50 -0700

    removed rand_index step and just applied sample to the string_vec itself

--- scripts/anagramr_dict.R
+++ scripts/anagramr_dict.R
@@ -1,6 +1,8 @@
 ## daily_r challenge 3: create a function that takes a string and outputs all possible combinations of those characters
 ## Bonus challenge - return only english words (at least those found in an r package dictionary)
 
+## This approach is atrociously inefficient
+
 library(tidyverse)
 library(qdapDictionaries)
 
@@ -18,8 +20,9 @@ num_permuatation <- function(value) {
 # iterating through the unique values in our vector so that the instances of each
 # character in the original vector are only evaluated once ----
   
-# gregexpr() looks for i in value and outputs a list representing indices in value 
-# where i was not present with a -1 and where it was, with a 1. reagmatches() then 
+# For loop below step by step:  
+# gregexpr() looks for i in value and outputs a list replacing indices in value 
+# where i was not present, with a -1, and where it was, with a 1. reagmatches() then 
 # replaces the 1's with i and removes all the -1's. lengths() then outputs a vector
 # with 1's in place of the i values and zeroes elsewhere. We take the sum of that 
 # and add that number (num) to our char_repeats vector. 
@@ -66,17 +69,12 @@ anagramr_dict <- function(string) {
   # using function from above
   permutations <- num_permuatation(string_vec)
   
-  # this while-loop will continue generating randomly sorted vectors of numbers 
-  # (with a length that is the number of charters in the given string) and using 
-  # those numbers as indices to create new string permutations until the length 
-  # of the vector we're populating hits the number of possible permutations. The 
-  # nested if statement prevents repeats from being added to the output vector.
+  # this while-loop will continue generating random arrangements of the string
+  # until the output vector length is the number of possible permutations
   
   while (length(anagram_vec) < permutations) {
     
-    rand_index <- sample(seq(1, nchar(string)), size = nchar(string))
-    
-    new_gram <- paste(string_vec[rand_index], collapse = "")
+    new_gram <- paste(sample(string_vec, size = length(string_vec)), collapse = "")
     
     if ((!new_gram %in% anagram_vec)){
       
